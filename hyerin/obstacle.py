97967e0cb607f9f685f0299379fe637cfc4a60c1
devkev00 <kimminsoo116@gmail.com>
2026-02-03 16:30:18 +0900

    feat: mod parameters

--- hyerin/obstacle.py
+++ hyerin/obstacle.py
@@ -6,7 +6,7 @@ import cv2
 
 from common_lane_base import (
     PORT, BAUDRATE, SERIAL_DELAY, SPEED_REFRESH_DELAY,
-    CAM_INDEX, MAX_SPEED, width, height,  # ❌ width/height는 common에 없으므로 아래에서 정의
+    CAM_INDEX, MAX_SPEED  # ❌ width/height는 common에 없으므로 아래에서 정의
 )
 
 from common_lane_base import (
